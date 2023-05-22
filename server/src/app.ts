c8fc08a00e0e068adcc1608c8726cdbd6bd14d0b
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-05-22 09:01:00 +0200

    add session middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
+import session from 'express-session';
 
 import * as middlewares from './middlewares';
 import api from './api';
@@ -16,13 +17,22 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+const salt = process.env.PASSKEYPOC_COOKIESALT;
+if (!salt) throw new Error('no cookiesalt in env var');
+
+app.use(session(
+  {
+    secret: 'blabla',
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: true,
+  }));
+
 app.get<{}, MessageResponse>('/', (req, res) => {
   res.json({
     message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
   });
 });
-
-
 
 app.use('/api/v1', api);
 
