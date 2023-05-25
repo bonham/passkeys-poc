f80d779e2a442b2a193528e5e883919506d074dd
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-05-25 08:44:26 +0200

    store to backend

--- server/src/api/auth.ts
+++ server/src/api/auth.ts
@@ -2,6 +2,23 @@ import express from 'express';
 import type { Request, Response, NextFunction } from 'express';
 import type { Session } from 'express-session';
 import { server } from '@passwordless-id/webauthn';
+import { RegistrationParsed } from '@passwordless-id/webauthn/dist/esm/types.js';
+import pg from 'pg';
+
+import * as dotenv from 'dotenv';
+const dotenvResult = dotenv.config();
+if (dotenvResult.error) {
+  console.error('Missing configuration: Please copy .env.sample to .env and modify config');
+  process.exit(1);
+}
+
+const pgpool = new pg.Pool({
+  host: process.env.PASSKEYPOC_PGHOST,
+  port: Number(process.env.PASSKEYPOC_PGPORT),
+  user: process.env.PASSKEYPOC_PGUSER,
+  password: process.env.PASSKEYPOC_PGPASSWORD,
+  database: process.env.PASSKEYPOC_PGDATABASE,
+});
 
 // import type { RegistrationParsed } from '@passwordless-id/webauthn/dist/esm/types';
 
@@ -16,10 +33,16 @@ function isAuthenticated(req: Request, res: Response, next: NextFunction) {
   else next('route');
 }
 
-function processRegistration(regParsed: any) {
+async function processRegistration(regParsed: RegistrationParsed) {
   const credential = regParsed.credential;
   console.log(`User ${regParsed.username} was registered`);
   console.log(credential);
+
+  const id = credential.id;
+  const credJson = JSON.stringify(credential);
+  const sql = `INSERT INTO public.credentialkey(id, data) VALUES ('${id}', '${credJson}');`;
+  const dbresult = await pgpool.query(sql);
+  return dbresult;
 }
 
 router.post('/register', async (req: RequestWithSession, res) => {
@@ -43,9 +66,10 @@ router.post('/register', async (req: RequestWithSession, res) => {
       challenge: storedChallenge,
       origin: 'http://localhost:5173',
     };
+    let registrationParsed: RegistrationParsed;
     try {
-      const registrationParsed = await server.verifyRegistration(registration, expected);
-      processRegistration(registrationParsed);
+      registrationParsed = await server.verifyRegistration(registration, expected);
+      await processRegistration(registrationParsed);
     } catch (e) {
       console.error('Registration failed:', e);
       res.sendStatus(400);
