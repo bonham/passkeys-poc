1f1d2924f084a93d60a11a0faf413f36e93cc2d0
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-05-28 00:09:44 +0200

    registration works

--- client/src/components/PassKeyFirst.vue
+++ client/src/components/PassKeyFirst.vue
@@ -1,70 +1,122 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { client } from '@passwordless-id/webauthn'
-import type { RegistrationEncoded, AuthenticationEncoded } from '@passwordless-id/webauthn/dist/esm/types';
+import { startRegistration } from '@simplewebauthn/browser';
+import type { RegistrationResponseJSON } from '@simplewebauthn/typescript-types'
 
-
-const visible = ref(false)
-const localAuthenticatorAvailable = ref(false)
 const regstatus = ref("None")
-const loginstatus = ref("None")
+// const loginstatus = ref("None")
 
-checkPrereqs()
+// checkPrereqs()
 
-async function checkPrereqs() {
+// async function checkPrereqs() {
 
-  if (client.isAvailable()) {
-    visible.value = true
+//   if (client.isAvailable()) {
+//     visible.value = true
+//   }
+
+//   if (await client.isLocalAuthenticator())
+//     localAuthenticatorAvailable.value = true
+// }
+
+// type ChallengeResponse = { challenge: string }
+
+// async function getChallenge(): Promise<string> {
+//   const url = 'http://localhost:5000/api/v1/auth/challenge'
+//   var resp: Response
+//   const opts: RequestInit = {
+//     method: 'GET',
+//     credentials: "include"
+//   }
+//   try {
+//     resp = await fetch(url, opts)
+//     if (!resp.ok) {
+//       throw new Error("Response not ok" + resp)
+//     } else {
+//       const respj: ChallengeResponse = await resp.json()
+//       return respj.challenge
+//     }
+//   } catch (e) {
+//     throw new Error("Network error during fetch" + e)
+//   }
+// }
+
+async function handleCreate() {
+
+  // GET registration options from the endpoint that calls
+  // @simplewebauthn/server -> generateRegistrationOptions()
+  const resp = await getWithCORS('/api/v1/auth/regoptions');
+
+  let attResp: RegistrationResponseJSON;
+  try {
+    // Pass the options to the authenticator and wait for a response
+    attResp = await startRegistration(await resp.json());
+  } catch (error) {
+
+    if (error instanceof Error) {
+      // Some basic error handling
+      if (error.name === 'InvalidStateError') {
+        regstatus.value = 'Error: Authenticator was probably already registered by user';
+      } else {
+        regstatus.value = error.name + " / " + error.message;
+      }
+    }
+    throw error;
   }
 
-  if (await client.isLocalAuthenticator())
-    localAuthenticatorAvailable.value = true
+  // POST the response to the endpoint that calls
+  // @simplewebauthn/server -> verifyRegistrationResponse()
+  const verificationResp = await sendJSONToServer('/api/v1/auth/register', JSON.stringify(attResp));
+
+  // Wait for the results of verification
+  const verificationJSON = await verificationResp.json();
+  console.log("verificationJson:", verificationJSON)
+
+  // Show UI appropriate for the `verified` status
+  if (verificationJSON && verificationJSON.verified) {
+    regstatus.value = 'Success!';
+  } else {
+    regstatus.value = `Oh no, something went wrong! Response: <pre>${JSON.stringify(
+      verificationJSON,
+    )}</pre>`;
+  }
 }
 
-type ChallengeResponse = { challenge: string }
 
-async function getChallenge(): Promise<string> {
-  const url = 'http://localhost:5000/api/v1/auth/challenge'
-  var resp: Response
+// async function handleCreate() {
+
+//   const challenge = await getChallenge()
+//   if (!challenge) throw new Error("Challenge not defined")
+//   const registration = await client.register("Arnaud", challenge, {
+//     "authenticatorType": "both",
+//     "userVerification": "required",
+//     "timeout": 60000,
+//     "attestation": false,
+//     "debug": false
+//   })
+//   console.log("reg", registration)
+//   let response: Response
+//   try {
+//     response = await sendRegToServer(registration)
+//   } catch (e) {
+//     console.log("Failure in registration call", e)
+//     regstatus.value = "Communication failure"
+//     return
+//   }
+//   const success: boolean = response.ok
+//   regstatus.value = success ? "Success" : "Failure"
+// }
+
+async function getWithCORS(path: string) {
+
+  const url = `http://localhost:5000${path}`
   const opts: RequestInit = {
     method: 'GET',
     credentials: "include"
   }
-  try {
-    resp = await fetch(url, opts)
-    if (!resp.ok) {
-      throw new Error("Response not ok" + resp)
-    } else {
-      const respj: ChallengeResponse = await resp.json()
-      return respj.challenge
-    }
-  } catch (e) {
-    throw new Error("Network error during fetch" + e)
-  }
-}
 
-async function handleCreate() {
-
-  const challenge = await getChallenge()
-  if (!challenge) throw new Error("Challenge not defined")
-  const registration = await client.register("Arnaud", challenge, {
-    "authenticatorType": "both",
-    "userVerification": "required",
-    "timeout": 60000,
-    "attestation": false,
-    "debug": false
-  })
-  console.log("reg", registration)
-  let response: Response
-  try {
-    response = await sendRegToServer(registration)
-  } catch (e) {
-    console.log("Failure in registration call", e)
-    regstatus.value = "Communication failure"
-    return
-  }
-  const success: boolean = response.ok
-  regstatus.value = success ? "Success" : "Failure"
+  // can throw error
+  const resp = await fetch(url, opts);
+  return resp
 }
 
 async function sendJSONToServer(path: string, payload: string) {
@@ -85,62 +137,56 @@ async function sendJSONToServer(path: string, payload: string) {
   return resp
 }
 
-async function sendRegToServer(registration: RegistrationEncoded) {
-  const uri = "/api/v1/auth/register"
-  const payload = JSON.stringify(registration)
-  return await sendJSONToServer(uri, payload)
-}
+// async function sendRegToServer(registration: RegistrationEncoded) {
+//   const uri = "/api/v1/auth/register"
+//   const payload = JSON.stringify(registration)
+//   return await sendJSONToServer(uri, payload)
+// }
 
-async function sendAuthDataToServer(authentication: AuthenticationEncoded) {
-  const uri = "/api/v1/auth/login"
-  const payload = JSON.stringify(authentication)
-  return await sendJSONToServer(uri, payload)
-}
+// async function sendAuthDataToServer(authentication: AuthenticationEncoded) {
+//   const uri = "/api/v1/auth/login"
+//   const payload = JSON.stringify(authentication)
+//   return await sendJSONToServer(uri, payload)
+// }
 
-async function handleLogin() {
-  const challenge = await getChallenge()
-  let authentication: AuthenticationEncoded
-  try {
-    authentication = await client.authenticate([], challenge, {
-      "authenticatorType": "both",
-      "userVerification": "required",
-      "timeout": 60000
-    })
-    console.log("auth", authentication)
-  } catch (e) {
-    console.log("Passkey selection failed", e)
-    loginstatus.value = "Passkey selection failed"
-    return
-  }
+// async function handleLogin() {
+//   const challenge = await getChallenge()
+//   let authentication: AuthenticationEncoded
+//   try {
+//     authentication = await client.authenticate([], challenge, {
+//       "authenticatorType": "both",
+//       "userVerification": "required",
+//       "timeout": 60000
+//     })
+//     console.log("auth", authentication)
+//   } catch (e) {
+//     console.log("Passkey selection failed", e)
+//     loginstatus.value = "Passkey selection failed"
+//     return
+//   }
 
-  let authResponse: Response
-  try {
-    authResponse = await sendAuthDataToServer(authentication)
-  } catch (e) {
-    console.error("Failure in authentication call", e)
-    loginstatus.value = "Communication failure"
-    return
-  }
+//   let authResponse: Response
+//   try {
+//     authResponse = await sendAuthDataToServer(authentication)
+//   } catch (e) {
+//     console.error("Failure in authentication call", e)
+//     loginstatus.value = "Communication failure"
+//     return
+//   }
 
-  loginstatus.value = authResponse.ok ? "Success" : "Unauthorized"
+//   loginstatus.value = authResponse.ok ? "Success" : "Unauthorized"
 
-}
+// }
 
 
 </script>
 
 <template>
   <div>Hello</div>
-  <div v-if="visible">
-    <button @click="handleCreate">Register</button>
-    <div>Local Authenticator
-      <div v-if="!localAuthenticatorAvailable">not</div>
-      available
-    </div>
-    <div>Registration status: {{ regstatus }}</div>
-    <div>
-      <button @click="handleLogin">Login</button>
-      <div>Login status: {{ loginstatus }}</div>
-    </div>
-  </div>
+  <button @click="handleCreate">Register</button>
+  <div>Registration status: {{ regstatus }}</div>
+  <!-- <div>
+                <button @click="handleLogin">Login</button>
+                <div>Login status: {{ loginstatus }}</div>
+              </div> -->
 </template>
--- server/src/api/auth.ts
+++ server/src/api/auth.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import type { Request, Response, NextFunction } from 'express';
 import type { Session } from 'express-session';
-import { server } from '@passwordless-id/webauthn';
-import type { AuthenticationEncoded, RegistrationParsed } from '@passwordless-id/webauthn/dist/esm/types.js';
 import pg from 'pg';
+import {
+  generateRegistrationOptions,
+  verifyRegistrationResponse,
+} from '@simplewebauthn/server';
+import type { UserModel, Authenticator } from './server.d.ts';
+
+// Human-readable title for your website
+const rpName = 'SimpleWebAuthn Example';
+// A unique identifier for your website
+const rpID = 'localhost';
+// The URL at which registrations and authentications should occur
+const origin = `http://${rpID}:5173`;
 
 import * as dotenv from 'dotenv';
-import { RegistrationEncoded } from '@passwordless-id/webauthn/dist/esm/types.js';
 const dotenvResult = dotenv.config();
 if (dotenvResult.error) {
   console.error('Missing configuration: Please copy .env.sample to .env and modify config');
@@ -21,12 +30,16 @@ const pgpool = new pg.Pool({
   database: process.env.PASSKEYPOC_PGDATABASE,
 });
 
-// import type { RegistrationParsed } from '@passwordless-id/webauthn/dist/esm/types';
-
 const router = express.Router();
 
-type RequestWithSession = Request & { session: Session };
-type MySession = Session & { user: string | undefined, challenge: string | undefined };
+interface MySession extends Session {
+  user?: string,
+  challenge?: string
+}
+
+interface RequestWithSession extends Request {
+  session: MySession
+}
 
 // middleware to test if authenticated
 function isAuthenticated(req: Request, res: Response, next: NextFunction) {
@@ -34,123 +47,207 @@ function isAuthenticated(req: Request, res: Response, next: NextFunction) {
   else next('route');
 }
 
-async function processRegistration(regParsed: RegistrationParsed) {
-  const credential = regParsed.credential;
-  console.log('Fullpayload:', regParsed);
-  console.log(`User ${regParsed.username} was registered`);
-  console.log(credential);
 
-  const id = credential.id;
-  const credJson = JSON.stringify(credential);
-  const sql = `INSERT INTO public.credentialkey(id, data) VALUES ('${id}', '${credJson}');`;
-  const dbresult = await pgpool.query(sql);
-  return dbresult;
-}
+async function getUserAuthenticators(user: UserModel) {
 
-async function getCredentials(credentialId: string) {
-  if (!credentialId) throw Error('credentialid is false');
-  const sql = `select data from public.credentialkey where id = '${credentialId}'`;
-  console.log('sql:', sql);
-  const res = await pgpool.query(sql);
-  if (res.rowCount > 1) throw Error('Row count is higher than 1');
-  if (res.rowCount === 0) return null;
-  const rawData = res.rows[0].data;
-  const credentials = JSON.parse(rawData) as {
-    id: string;
-    publicKey: string;
-    algorithm: 'RS256' | 'ES256';
+  const query = {
+    text: 'SELECT credentialID, credentialPublicKey, counter, credentialDeviceType, credentialBackedUp, transports FROM public.cred_authenticators where userid = $1',
+    values: [user.id],
   };
-  return credentials;
+  const res = await pgpool.query(query);
+  const authenticators: Authenticator[] = res.rows;
+  return authenticators;
 }
+
+// currently logged in user ( for registration )
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function getUserFromDB(userid: string) {
+  const user: UserModel = {
+    id: 'user1',
+    username: 'Paul',
+    currentChallenge: 'what',
+  };
+  return user;
+}
+
+router.get('/regoptions', async (req: RequestWithSession, res) => {
+
+  // (Pseudocode) Retrieve the user from the database
+  // after they've logged in
+  // const user: UserModel = getUserFromDB(loggedInUserId);
+  const user = await getUserFromDB('fake');
+
+  // (Pseudocode) Retrieve any of the user's previously-
+  // registered authenticators
+  const userAuthenticators: Authenticator[] = await getUserAuthenticators(user);
+
+  const options = generateRegistrationOptions({
+    rpName,
+    rpID,
+    userID: user.id,
+    userName: user.username,
+    // Don't prompt users for additional information about the authenticator
+    // (Recommended for smoother UX)
+    attestationType: 'none',
+    // Prevent users from re-registering existing authenticators
+    excludeCredentials: userAuthenticators.map(authenticator => ({
+      id: authenticator.credentialID,
+      type: 'public-key',
+      // Optional
+      transports: authenticator.transports,
+    })),
+  });
+
+  // (Pseudocode) Remember the challenge for this user
+  req.session.challenge = options.challenge;
+  res.json(options);
+
+});
+
+// async function processRegistration(regParsed: RegistrationParsed) {
+//   const credential = regParsed.credential;
+//   console.log('Fullpayload:', regParsed);
+//   console.log(`User ${regParsed.username} was registered`);
+//   console.log(credential);
+
+//   const id = credential.id;
+//   const credJson = JSON.stringify(credential);
+//   const sql = `INSERT INTO public.credentialkey(id, data) VALUES ('${id}', '${credJson}');`;
+//   const dbresult = await pgpool.query(sql);
+//   return dbresult;
+// }
+
+// async function getCredentials(credentialId: string) {
+//   if (!credentialId) throw Error('credentialid is false');
+//   const sql = `select data from public.credentialkey where id = '${credentialId}'`;
+//   console.log('sql:', sql);
+//   const res = await pgpool.query(sql);
+//   if (res.rowCount > 1) throw Error('Row count is higher than 1');
+//   if (res.rowCount === 0) return null;
+//   const rawData = res.rows[0].data;
+//   const credentials = JSON.parse(rawData) as {
+//     id: string;
+//     publicKey: string;
+//     algorithm: 'RS256' | 'ES256';
+//   };
+//   return credentials;
+// }
 
 router.post('/register', async (req: RequestWithSession, res) => {
-  const registration = req.body as RegistrationEncoded;
-  const session = (req.session as MySession);
-
-  // read and save challenge
-  const storedChallenge = session.challenge;
-
-  // invalidate challenge to avoid it is used twice
-  session.challenge = undefined;
-
-  if (storedChallenge === undefined) {
-    console.log('Stored challenge not defined - why?');
-    res.sendStatus(403);
-    res.end();
-  } else {
-
-    // verify registration
-    const expected = {
-      challenge: storedChallenge,
-      origin: 'http://localhost:5173',
-    };
-    let registrationParsed: RegistrationParsed;
-    try {
-      registrationParsed = await server.verifyRegistration(registration, expected);
-      await processRegistration(registrationParsed);
-    } catch (e) {
-      console.error('Registration failed:', e);
-      res.sendStatus(400);
-    }
-  }
-  res.send();    // echo the result back
-});
-
-
-router.post('/login', async (req: RequestWithSession, res: Response) => {
-  const session = (req.session as MySession);
-  const authentication = req.body as AuthenticationEncoded;
-  const credId = authentication.credentialId;
-  if (credId === undefined) {
-    res.status(400).send('Could not find credential id in payload');
-  }
-
-  const credentialKey = await getCredentials(credId);
-  if (credentialKey === null) {
-    console.log('Credentialkey not found in db');
+  //const user = await getUserFromDB('fake');
+  const expectedChallenge = req.session.challenge;
+  if (expectedChallenge === undefined) {
+    console.log('Current user challenge is undefined');
     res.sendStatus(401);
-    return; // satisfy ts
-  }
-  const storedChallenge = session.challenge;
-
-
-  if (storedChallenge === undefined) {
-    console.log('Stored challenge not defined - why?');
-    res.sendStatus(401);
-
-  } else {
-
-    const expected = {
-      challenge: storedChallenge,
-      origin: 'http://localhost:5173',
-      userVerified: true,
-      counter: 0,
-    };
-
-    try {
-      const authenticationParsed = await server.verifyAuthentication(authentication, credentialKey, expected);
-      console.log('Auth parsed:', authenticationParsed);
-      session.user = authenticationParsed.credentialId;
-      res.sendStatus(200);
-    } catch (e) {
-      console.error('Authentication failed:', e);
-      res.sendStatus(401);
-    }
+    return;
   }
 
+  const { body } = req;
+
+  let verification;
+  try {
+    verification = await verifyRegistrationResponse({
+      response: body,
+      expectedChallenge,
+      expectedOrigin: origin,
+      expectedRPID: rpID,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send({ error: (error as any).message }); // dirty
+  }
+
+  res.json(verification);
 });
 
-router.get('/challenge', (req: RequestWithSession, res) => {
+// router.post('/register', async (req: RequestWithSession, res) => {
+//   const registration = req.body as RegistrationEncoded;
+//   const session = (req.session as MySession);
 
-  const session = (req.session as MySession);
+//   // read and save challenge
+//   const storedChallenge = session.challenge;
 
-  // store challenge to session before returning
-  const challenge = crypto.randomUUID();
-  session.challenge = challenge;
+//   // invalidate challenge to avoid it is used twice
+//   session.challenge = undefined;
 
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ challenge }));
-});
+//   if (storedChallenge === undefined) {
+//     console.log('Stored challenge not defined - why?');
+//     res.sendStatus(403);
+//     res.end();
+//   } else {
+
+//     // verify registration
+//     const expected = {
+//       challenge: storedChallenge,
+//       origin: 'http://localhost:5173',
+//     };
+//     let registrationParsed: RegistrationParsed;
+//     try {
+//       registrationParsed = await server.verifyRegistration(registration, expected);
+//       await processRegistration(registrationParsed);
+//     } catch (e) {
+//       console.error('Registration failed:', e);
+//       res.sendStatus(400);
+//     }
+//   }
+//   res.send();    // echo the result back
+// });
+
+
+// router.post('/login', async (req: RequestWithSession, res: Response) => {
+//   const session = (req.session as MySession);
+//   const authentication = req.body as AuthenticationEncoded;
+//   const credId = authentication.credentialId;
+//   if (credId === undefined) {
+//     res.status(400).send('Could not find credential id in payload');
+//   }
+
+//   const credentialKey = await getCredentials(credId);
+//   if (credentialKey === null) {
+//     console.log('Credentialkey not found in db');
+//     res.sendStatus(401);
+//     return; // satisfy ts
+//   }
+//   const storedChallenge = session.challenge;
+
+
+//   if (storedChallenge === undefined) {
+//     console.log('Stored challenge not defined - why?');
+//     res.sendStatus(401);
+
+//   } else {
+
+//     const expected = {
+//       challenge: storedChallenge,
+//       origin: 'http://localhost:5173',
+//       userVerified: true,
+//       counter: 0,
+//     };
+
+//     try {
+//       const authenticationParsed = await server.verifyAuthentication(authentication, credentialKey, expected);
+//       console.log('Auth parsed:', authenticationParsed);
+//       session.user = authenticationParsed.credentialId;
+//       res.sendStatus(200);
+//     } catch (e) {
+//       console.error('Authentication failed:', e);
+//       res.sendStatus(401);
+//     }
+//   }
+
+// });
+
+// router.get('/challenge', (req: RequestWithSession, res) => {
+
+//   const session = (req.session as MySession);
+
+//   // store challenge to session before returning
+//   const challenge = crypto.randomUUID();
+//   session.challenge = challenge;
+
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(JSON.stringify({ challenge }));
+// });
 
 router.get('/check', (req: (Request & { session: Session }), res) => {
   const user = (req.session as MySession).user;
