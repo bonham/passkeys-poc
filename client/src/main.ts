5479a9657ec2e17548b629cc69df47dcc22e7296
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-05-30 23:44:14 +0200

    add bootstrap

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as bootstrap from 'bootstrap'
 
 createApp(App).mount('#app')
