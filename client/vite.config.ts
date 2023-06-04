5ef06bd8ae7152ba352deb4347d72290ee553aa4
Art Furrer <2823246+bonham@users.noreply.github.com>
2023-06-04 22:14:26 +0200

    backend proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -11,5 +11,16 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000'
+      }
+    }
   }
 })
